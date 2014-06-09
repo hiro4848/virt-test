10a182b7e6c95667873e6ac8c97619d887a4a94f
Takehiro Takahashi <takehiro.takahashi@gmail.com>
2014-06-08 21:03:27 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VirtTest::Application.config.secret_token = '0370b3ce7f0cda448806e09e0814948affef2694337e0c5cbd8c8639fbaad5f576d4b6c111fa8fea32c15d230c22b90ac2b02115b320c180bcf4f22551cd3b69'
